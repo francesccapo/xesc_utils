4499f8702e1fa8b4b22125ff2ab28fef9a60e5cb
Francesc Capó Clar <fcapo@digitalorigin.com>
2018-01-15 11:53:52 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+import distutils.core
+
+distutils.core.setup(
+    name='xesc',  # This is the name of your PyPI-package.
+    version='1.1',  # Update the version number for new releases
+    packages=['xesc'],  # The name of your scipt, and also the command you'll be using for calling it
+    include_package_data=True, requires=['pandas', 'numpy', 'matplotlib', 'plotly']
+)
